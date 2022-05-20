407df2569b60607db5dde921b146f2250260a224
Rafaello <baimurzinrafael02@gmail.com>
2022-05-20 22:46:28 +0600

    Created TaskViewModel

---
+++ app/src/main/java/com/rafael/todoister/model/TaskViewModel.java
@@ -0,0 +1,44 @@
+package com.rafael.todoister.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TaskViewModel extends AndroidViewModel {
+
+    private static DatabaseReference reference;
+
+    public TaskViewModel(@NonNull Application application) {
+        super(application);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        String userId = user.getUid();
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Tasks").child(userId);
+    }
+
+    public static com.google.android.gms.tasks.Task<Void> save(Task task) {
+        task.setId(reference.push().getKey());
+        return reference.child(task.getId()).setValue(task);
+    }
+
+    public static com.google.android.gms.tasks.Task<Void> update(Task task) {
+        return reference.child(task.getId()).setValue(task);
+    }
+
+    public static com.google.android.gms.tasks.Task<Void> delete(String key) {
+        return reference.child(key).removeValue();
+    }
+
+    public static DatabaseReference getReference() {
+        return reference;
+    }
+
+}
