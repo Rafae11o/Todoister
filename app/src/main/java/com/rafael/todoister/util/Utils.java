0b59e853819c14632768509c7a98ab08e1866a7d
Rafaello <baimurzinrafael02@gmail.com>
2022-05-20 22:48:14 +0600

    Created Util

---
+++ app/src/main/java/com/rafael/todoister/util/Utils.java
@@ -0,0 +1,47 @@
+package com.rafael.todoister.util;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.rafael.todoister.model.Priority;
+import com.rafael.todoister.model.Task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+
+    public static String formatDate(Date date) {
+        SimpleDateFormat simpleDateFormat = (SimpleDateFormat) SimpleDateFormat.getDateInstance();
+        simpleDateFormat.applyPattern("EEE, MMM d");
+        return simpleDateFormat.format(date);
+    }
+
+    public static String formatDate(Date date, String datePattern) {
+        SimpleDateFormat simpleDateFormat = (SimpleDateFormat) SimpleDateFormat.getDateInstance();
+        simpleDateFormat.applyPattern(datePattern);
+        return simpleDateFormat.format(date);
+    }
+
+    public static void hideSoftKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(
+                Context.INPUT_METHOD_SERVICE
+        );
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static int priorityColor(Task task) {
+        int color;
+        if(task.getPriority() == Priority.HIGH) {
+            color = Color.argb(200, 201, 21, 23);
+        } else if(task.getPriority() == Priority.MEDIUM) {
+            color = Color.argb(200, 155, 179, 0);
+        } else {
+            color = Color.argb(200, 51, 181, 129);
+        }
+        return color;
+    }
+
+}
